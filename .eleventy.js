3084da135102da3aa8656fdaf6ddd88e55ded9ee
Fredrik Solås Arnstad <fredrikarnstad@Fredrik.local>
2025-10-31 19:30:47 +0100

    added img transform plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,21 @@
-module.exports = function(eleventyConfig) {
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
+export default function (eleventyConfig) {
 	eleventyConfig.addCollection('hvem', function(collection) {
 		return collection.getAll().filter(post => post.data.hvem)
 	})
+
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    formats: ['webp'],
+    widths: ['auto'],
+    htmlOptions: {
+      imgAttributes: {
+        loading: 'lazy',
+        decoding: 'async',
+      }
+    },
+    pictureAttributes: {}
+  })
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 	eleventyConfig.addPassthroughCopy('./src/assets')
---
+++ src/gifts/nintendo-switch-2.md
@@ -0,0 +1,8 @@
+---
+title: Nintendo Switch 2
+hvem: Fredrik
+img: /assets/img/nintendo-switch-2.avif
+externalUrl: 
+tags:
+  - gave
+---
